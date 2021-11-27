b5577081412ce01db3e2290e5d9e811a1917b3fc
thiagoatf@hotmail.com <thiagoatf@hotmail.com>
2021-11-27 18:55:40 -0300

    Add project files.

---
+++ ConversorDolar/Entities/Conversor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConversorDolar
+
+{
+    class Conversor
+    {
+        //Variáveis e uso do encapsulamento
+        public double Dolar { get; private set; }
+        public double Cotacao { get; private set;}
+       
+        //Construtor vazio
+        public Conversor()
+        {
+        }
+
+        //Construtor personalizado
+        public Conversor(double dolar, double cotacao)
+        {
+            Dolar = dolar;
+            Cotacao = cotacao;            
+        }
+
+        //Método de conversão sem IOF
+        public double ConversaoSemIOF()
+        {
+            double Final = Dolar * Cotacao;
+            return Final;
+        }
+
+        //Método de conversão sem IOF
+        public double ConversaoComIOF()
+        {
+            return ((ConversaoSemIOF() / 100.00) * 4.08) + ConversaoSemIOF();            
+        }
+
+        
+    }
+}
---
+++ ConversorDolar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversorDolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Usuário digita a cotação e os dolares que ele quer converter
+            Console.WriteLine("Qual é a cotação do dólar?");
+            Console.Write("R$ ");
+            double cot = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantos dólares você irá comprar?");
+            Console.Write("$");
+            double dol = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Conversor conversor = new Conversor(dol, cot);
+
+            //Impressão da conversão sem e com IOF
+            Console.WriteLine("Valor a ser pago em reais = R$ " + conversor.ConversaoSemIOF());
+            Console.WriteLine("Valor a ser pago de IOF = 4,08%");
+            Console.WriteLine("Valor total da compra com IOF = R$ " + conversor.ConversaoComIOF());
+            
+        }
+    }
+}
